446ed26a0da26798031dd6a6de50f25f184786f9
Houstin <houstin.angwenyi@student.moringaschool.com>
2023-07-23 19:07:35 +0300

    successfully render list of transactions on table and fetched data from api

--- src/TransactionsTable.js
+++ src/TransactionsTable.js
@@ -1,5 +1,5 @@
 import React,{useState,useEffect} from "react"
-const TransactionsTable= ()=>{
+function TransactionsTable (){
     const [transactions,setTransactions]=useState([])
     
     useEffect(()=>{
@@ -7,45 +7,35 @@ const TransactionsTable= ()=>{
     },[])
     
     const fetchTransactions=() =>{
-
         fetch("https://animeworld-lgf0.onrender.com/transactions")
         .then((response)=> response.json())
         .then((data)=>{
-            console.log(data);
             setTransactions(data)
         })
         .catch((error)=>{
             console.error('Error fetching transactions',error);
         })
     }
-   // let transactionElement=[]
-    if(transactions &&transactions.length>0){
-        return (transactions.map((transaction) => (
+
+  return (
         <table>
             <thead>
             <tr>
+                <th>Date</th>
               <th>Description</th>
               <th>Category</th>
                <th>Amount</th>
             </tr>
             </thead>
-        
         <tbody>
+            {transactions.map((transaction) => (
             <tr key={transaction.id}>
+              <td>{transaction.date}</td>
               <td>{transaction.description}</td>
               <td>{transaction.category}</td>
               <td>{transaction.amount}</td>
             </tr>
-        </tbody>
-        </table>
-
-          ))
-        )
-    }
-
-
-    
-       
-  
+        ))}</tbody>
+        </table>) 
 }
 export default TransactionsTable
